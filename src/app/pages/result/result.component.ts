b81ac31bd0b40273958ebe75d6a1b402911f05a1
twodzinski@op.pl <twodzinski@op.pl>
2023-10-02 00:36:08 +0200

    added form with validation to check result

--- src/app/pages/result/result.component.ts
+++ src/app/pages/result/result.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpLottoService} from "../../services/http-lotto.service";
+import {TicketIdRequest} from "../../models/ticketidrequest";
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.css']
 })
-export class ResultComponent {
+export class ResultComponent{
+
+  model: Partial<TicketIdRequest> = {};
+  errorMessage: string;
+  constructor(private http: HttpLottoService) {
+  }
+
+  getResultById(){
+    this.errorMessage = '';
+    this.http.getResultByTicketId(this.model.ticketId as string).subscribe(
+     {error: err => this.errorMessage = err}
+   );
+  }
 
 }
